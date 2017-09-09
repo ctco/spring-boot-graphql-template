fba81b2214674c7c913bf9d56d56e5d6279e6159
trioletas <olegs.gabrusjonoks@gmail.com>
2017-09-09 21:38:20 +0300

    Library update

--- api/src/main/java/lv/ctco/tpl/bff/graphql/Query.java
+++ api/src/main/java/lv/ctco/tpl/bff/graphql/Query.java
@@ -1,21 +1,21 @@
 package lv.ctco.tpl.bff.graphql;
 
-import com.coxautodev.graphql.tools.GraphQLRootResolver;
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import lv.ctco.tpl.bff.graphql.resolvers.JokeResolver;
 import lv.ctco.tpl.bff.graphql.types.Joke;
 import lv.ctco.tpl.bff.graphql.types.JokeCategory;
+import lv.ctco.tpl.bff.graphql.types.JokeCategoryInput;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Query implements GraphQLRootResolver {
+public class Query implements GraphQLQueryResolver {
 
     @Autowired
     private JokeResolver jokeResolver;
 
-    public Joke jokeByCategory(JokeCategory category) {
-        return jokeResolver.getJokeByCategory(category);
-
+    public Joke jokeByCategory(JokeCategoryInput input) {
+        return jokeResolver.getJokeByCategory(input.getCategory());
     }
 
     public Joke jokeById(String id) {
