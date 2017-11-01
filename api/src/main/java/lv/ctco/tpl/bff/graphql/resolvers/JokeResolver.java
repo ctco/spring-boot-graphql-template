a9627235a08c6e5bdc183d4e39574a91fc80d541
Vadim Kuznecov <Vadim_Kuznecov@rcomext.com>
2017-11-01 16:49:12 +0100

    --changed to single

--- api/src/main/java/lv/ctco/tpl/bff/graphql/resolvers/JokeResolver.java
+++ api/src/main/java/lv/ctco/tpl/bff/graphql/resolvers/JokeResolver.java
@@ -9,7 +9,7 @@ import lv.ctco.tpl.bff.integration.icndb.jokes.JokeResponseModel;
 import lv.ctco.tpl.bff.integration.icndb.jokes.JokeValueModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import rx.Observable;
+import rx.Single;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
@@ -24,16 +24,14 @@ public class JokeResolver implements GraphQLResolver<JokeQuery> {
 
     public CompletableFuture<Joke> getJokeByCategory(JokeQuery root, JokeCategoryInput request) {
         JokeCategory category = request.getCategory();
-        Observable<JokeResponseModel> searchCommand = category != null ? icndb.getRandomJokeByCategory(category) : icndb.getRandomJoke();
+        Single<JokeResponseModel> searchCommand = category != null ? icndb.getRandomJokeByCategory(category) : icndb.getRandomJoke();
         return toCompletableFuture(searchCommand
-            .map(jokeValue -> mapToJoke(jokeValue.getValue()))
-            .toSingle());
+            .map(jokeValue -> mapToJoke(jokeValue.getValue())));
     }
 
     public CompletableFuture<Joke> getJokeById(JokeQuery root, String id) {
         return toCompletableFuture(icndb.getJokeById(id)
-            .map(response -> mapToJoke(response.getValue()))
-            .toSingle());
+            .map(response -> mapToJoke(response.getValue())));
     }
 
     private Joke mapToJoke(JokeValueModel jokeValue) {
--- api/src/main/java/lv/ctco/tpl/bff/integration/icndb/ICNDB.java
+++ api/src/main/java/lv/ctco/tpl/bff/integration/icndb/ICNDB.java
@@ -4,20 +4,20 @@ import feign.Param;
 import feign.RequestLine;
 import lv.ctco.tpl.bff.graphql.types.JokeCategory;
 import lv.ctco.tpl.bff.integration.icndb.jokes.JokeResponseModel;
-import rx.Observable;
+import rx.Single;
 
 public interface ICNDB {
 
     @RequestLine("GET /jokes/random")
-    Observable<JokeResponseModel> getRandomJoke();
+    Single<JokeResponseModel> getRandomJoke();
 
     @RequestLine(value = "GET /jokes/random?limitTo={category}", decodeSlash = false)
-    Observable<JokeResponseModel> getRandomJokeByCategory(
+    Single<JokeResponseModel> getRandomJokeByCategory(
         @Param(value = "category", expander = CategoryExpander.class) JokeCategory category
     );
 
     @RequestLine(value = "GET /jokes/{id}")
-    Observable<JokeResponseModel> getJokeById(
+    Single<JokeResponseModel> getJokeById(
         @Param(value = "id") String id
     );
 
