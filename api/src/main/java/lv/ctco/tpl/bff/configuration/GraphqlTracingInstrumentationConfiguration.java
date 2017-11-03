f6a9db4b94ca6fd14735b22eb63ac4e695b3f51a
Vadim Kuznecov <Vadim_Kuznecov@rcomext.com>
2017-11-03 13:19:45 +0100

    #5 Query tracing --code review changes

--- api/src/main/java/lv/ctco/tpl/bff/configuration/GraphqlTracingInstrumentationConfiguration.java
+++ api/src/main/java/lv/ctco/tpl/bff/configuration/GraphqlTracingInstrumentationConfiguration.java
@@ -5,26 +5,34 @@ import graphql.ExecutionResult;
 import graphql.execution.instrumentation.parameters.InstrumentationExecutionParameters;
 import graphql.execution.instrumentation.tracing.TracingInstrumentation;
 import graphql.execution.instrumentation.tracing.TracingSupport;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.Logger;
 
 @Component
-public class GraphqlTracingInstrumentation extends TracingInstrumentation {
+@Slf4j
+public class GraphqlTracingInstrumentationConfiguration extends TracingInstrumentation {
 
     private static final String DURATION = "duration";
-    private final Logger log = Logger.getLogger(GraphqlTracingInstrumentation.class.getSimpleName());
 
     @Override
     public CompletableFuture<ExecutionResult> instrumentExecutionResult(ExecutionResult executionResult,
                                                                         InstrumentationExecutionParameters parameters) {
+        if (log.isDebugEnabled()) {
+            logTracingResults(parameters);
+        }
+        return super.instrumentExecutionResult(executionResult, parameters);
+    }
+
+    private void logTracingResults(InstrumentationExecutionParameters parameters) {
         TracingSupport tracingSupport = parameters.getInstrumentationState();
         Long executionTime = getTime(tracingSupport.snapshotTracingData());
-        log.info(String.format("Query (%s) Execution time : %sms", parameters.getOperation(), executionTime));
-        return super.instrumentExecutionResult(executionResult, parameters);
+        if (executionTime != null) {
+            log.debug(String.format("Query (%s) Execution time : %s ms", parameters.getOperation(), executionTime));
+        }
     }
 
     private Long getTime(Map<String, Object> tracingData) {
